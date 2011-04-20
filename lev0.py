367943a8b2baf776664aebfe9bd127ad6ba217ce
Alex Vidal <alex.vidal@gmail.com>
2011-04-20 16:26:54 -0500

    Levels start at 0.

---
+++ lev0.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#http://www.pythonchallenge.com/pc/def/0.html
+
+from pc import url
+
+print url.format(2**38)
--- lev1.py
+++ lev1.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
-#http://www.pythonchallenge.com/pc/def/0.html
+#http://www.pythonchallenge.com/pc/def/map.html
 
 from pc import url
 
-print url.format(2**38)
+import string
+
+s = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+# this isn't the completely correct translation table, but it works
+trans = string.maketrans(string.lowercase, ''.join([chr(ord(c)+2) for c in string.lowercase]))
+
+print s.translate(trans)
+
+print url.format('map'.translate(trans))
